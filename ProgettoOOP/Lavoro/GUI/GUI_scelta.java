c450f75c2f02d0b6bc8f9109761f1bd0ea6d9963
ThomasBonacci <77808546+ThomasBonacci@users.noreply.github.com>
2021-01-21 21:59:35 +0100

    Update GUI_scelta.java

--- ProgettoOOP/Lavoro/GUI/GUI_scelta.java
+++ ProgettoOOP/Lavoro/GUI/GUI_scelta.java
@@ -6,11 +6,12 @@ import java.awt.Font;
 import java.awt.GridLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-
 import javax.swing.JButton;
 import javax.swing.JFrame;
-
 import javax.swing.JPanel;
+
+//@author Giacomo Lucesoli
+//@author Thomas Bonacci 
 
 public class GUI_scelta 
 {
